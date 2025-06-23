d3e2e2a1265310b8468d1af278cf24c282f74c04
odrgues <odrgues@gmail.com>
2025-06-23 16:41:51 -0300

    feat: remove conclude button from TaskCard component for streamlined UI

--- src/components/TaskCard/index.jsx
+++ src/components/TaskCard/index.jsx
@@ -23,27 +23,6 @@ const TaskCard = ({
         <h4>{titulo}</h4>
         <h5>{descricao}</h5>
         {concluida && <span className="concluida-label">Concluída</span>}
-        {!concluida && onConclude && (
-          <button
-            className="concluir-btn"
-            onClick={(e) => {
-              e.stopPropagation();
-              onConclude();
-            }}
-            style={{
-              marginTop: 8,
-              background: primaryColor,
-              color: "#fff",
-              border: "none",
-              borderRadius: 8,
-              padding: "4px 12px",
-              cursor: "pointer",
-              fontSize: 14,
-            }}
-          >
-            Concluir
-          </button>
-        )}
       </div>
     </div>
   );
