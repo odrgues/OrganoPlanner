8dddb1a65ae000eac4ce859d67d7ba93a52c3f0b
odrgues <odrgues@gmail.com>
2025-06-23 17:28:46 -0300

    feat: restructure Footer component layout and enhance styling for better responsiveness

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,25 +4,31 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <img src="/images/Footer.png" alt="Imagem de rodapé do Organo"></img>
-      <div className="footer-links">
-        <a
-          href="https://github.com/odrgues"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub Profile
-        </a>
-        <span className="footer-separator">|</span>
-        <a
-          href="https://github.com/RafaelPalomino18"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub Profile
-        </a>
-        <span className="footer-separator">|</span>
-        <Link to="/about">Sobre nós</Link>
+      <div className="footer-content">
+        <div className="footer-links">
+          <a
+            href="https://github.com/odrgues"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub Profile
+          </a>
+          <span className="footer-separator">|</span>
+          <a
+            href="https://github.com/RafaelPalomino18"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub Profile
+          </a>
+          <span className="footer-separator">|</span>
+          <Link to="/about">Sobre nós</Link>
+        </div>
+        <img
+          src="/images/Footer.png"
+          alt="Imagem de rodapé do Organo"
+          className="footer-img"
+        />
       </div>
     </footer>
   );
